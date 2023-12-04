ea4e9d53f6089c17f02e41a666e33fe37255d260
wwwru <www.rufat@bk.ru>
2023-12-04 11:04:27 +0300

    Start

--- mailings/models.py
+++ mailings/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    Email = models.EmailField(verbose_name='Почта',max_length=254, unique=True)
+    first_name = models.CharField(verbose_name='Имя', max_length=100)
+    last_name = models.CharField(verbose_name='Фамилия', max_length=100)
+    surname = models.CharField(verbose_name='Отчетсво', max_length=100)
+    comment = models.TextField(verbose_name='Комментарий', null=True, blank=True)
+    def __str__(self):
+        return f'{self.first_name}, {self.last_name}, {self.surname}'
+
+    class Meta:
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
+
+
